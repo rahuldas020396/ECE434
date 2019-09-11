b3ec89e9b0d73d29f104f3861cc8759dfe19958d
rahuldas020396 <dasrs@rose-hulman.edu>
2019-09-11 12:40:18 -0400

    Added Comments to my etch a sketch code

--- hw01/etch.py
+++ hw01/etch.py
@@ -1,24 +1,45 @@
+# Rahul Das
+# ECE 434
+# Homework 1
+# Etch a sketch game
+
+#Importing pygame
 import pygame,sys
 from pygame.locals import *
+
+#Initialize pygame
 pygame.init()
+
+# Setup window for etch a sketch game
 window_size = 500
 screen = pygame.display.set_mode((window_size,window_size))
 
+#Initial position of cursor
 x = 250
 y = 250
+
+# Setting up boolean two color palettes for the game
 color_switch = 0
 
+#Initial color for the cursor
 pointer_r = 255
 pointer_g = 255
 pointer_b = 255
 
+# setting up timer for game
 clock = pygame.time.Clock()
+
+# Initially black background
 screen.fill((0,0,0))
 
 while 1:
 	clock.tick(60)
+
+	#Creating the cursor and drawing the lines
 	pygame.draw.circle(screen,(pointer_r,pointer_g,pointer_b),(x,y),5)
 	pygame.display.update()
+
+	#Getting key presses and setting up boundaries for movement
 	key = pygame.key.get_pressed()
 
 	if key[pygame.K_RIGHT]:x+=5
@@ -30,14 +51,15 @@ while 1:
 	if key[pygame.K_DOWN]: y+=5
 	if y >= window_size: y = window_size
 
+	#Setting up what different key presses do
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			sys.exis()
-		elif event.type == KEYDOWN and event.key == K_ESCAPE:
 			sys.exit()
-		elif event.type == KEYDOWN and event.key == K_SPACE:
+		elif event.type == KEYDOWN and event.key == K_ESCAPE: # esc = quit
+			sys.exit()
+		elif event.type == KEYDOWN and event.key == K_SPACE: # space = clear
 			screen.fill((0,0,0))
-
+			#Preserving current color palette
 			if color_switch == 0:
 				screen.fill((0,0,0))
 				pointer_r = 255
@@ -49,7 +71,7 @@ while 1:
 				pointer_g = 0
 				pointer_b = 0
 
-		elif event.type == KEYDOWN and event.key == K_m:
+		elif event.type == KEYDOWN and event.key == K_m: # m = switch color palette
 			if color_switch == 0:
 				screen.fill((255,255,255))
 
@@ -66,5 +88,3 @@ while 1:
 				pointer_b = 255
 
 				color_switch = 0
-
-
