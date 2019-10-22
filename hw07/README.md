b87f137a56333159e461ca4de6b8a770b2b3bc3f
Demo User <debian@beaglebone.localdomain>
2019-10-22 16:47:50 +0000

    Blynk is done

---
+++ hw07/README.md
@@ -0,0 +1,10 @@
+The Blynk app has been downloaded and works with the board:
+1. The button on the Blynk app turns the on board LED on and off
+2. The button on the board turns the LED on the Blynk app on and off
+3. The slider on the Blynk app changes the intensity of the on board LED.
+
+Connected the LED to P9_14
+Connected the button to P9_25
+
+Wiki page for the coffee machine:
+https://elinux.org/ECE497_Project_Not_Your_Average_Cup_of_Joe
---
+++ hw07/leds.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+// Blinks various LEDs
+const Blynk = require('blynk-library');
+const b = require('bonescript');
+const util = require('util');
+
+const LED1 = 'P9_14';
+const LED0 = 'USR3';
+const button = 'P9_25';
+b.pinMode(LED0, b.OUTPUT);
+b.pinMode(LED1, b.OUTPUT);
+b.pinMode(button, b.INPUT);
+
+const AUTH = 'kTaeHf41Io_zAg5lhKgmgqrBnspS3g4g';
+
+
+var blynk = new Blynk.Blynk(AUTH);
+
+var v0 = new blynk.VirtualPin(0);
+var v10 = new blynk.WidgetLED(10);
+var v1 = new blynk.VirtualPin(1);
+
+
+v0.on('write', function(param) {
+    console.log('V0:', param[0]);
+    b.digitalWrite(LED0, param[0]);
+});
+
+
+v1.on('write', function(param) {
+    var duty_cycle =param[0]/1023;
+    b.analogWrite(LED1, duty_cycle);
+});
+
+
+
+v10.setValue(0);    // Initiallly off
+
+b.attachInterrupt(button, toggle, b.CHANGE);
+
+function toggle(x) {
+    console.log("V10: ", x.value);
+    x.value ? v10.turnOff() : v10.turnOn();
+}
